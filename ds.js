52da9030e25b4f89aad3a475a9e82815125ebdc8
aron herso <aronherso@gmail.com>
2023-11-21 12:35:59 +0530

    commit

---
+++ ds.js
@@ -0,0 +1,26 @@
+function free(){
+    alert("you want to trial?")
+}
+function subscribe(){
+    alert("Thank you for yours subcription")
+}
+// function price() {
+//     document.getElementById("myprice").classList.toggle("show");
+//   }
+  
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.Pricing')) {
+//       var dropdowns = document.getElementsByClassName("pricing-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openlink5 = dropdowns[i];
+//         if (openlink5.classList.contains('show')) {
+//           openlink5.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
+function price() {
+  document.getElementById("card").innerHTML="book"
+}
